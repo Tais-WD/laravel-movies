4bce80c4a5ab1c8bd1214f91b3c42ae19eebb94a
j_polak <jan.polak@onmyway.cz>
2019-06-06 10:22:10 +0200

    day 29 workout

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -49,4 +49,38 @@ class MovieController extends Controller
 
         return $view;
     }
+
+    public function detail()
+    {
+        // 16. - prepare the data (pretend to take it from DB)
+        $movie_title = 'Venom or how I became a monster';
+
+
+        // 9.
+        $info_view = view('movies/detail/info', [
+            'title' => $movie_title
+        ]);
+
+        // 10.
+        $cast_view = view('movies/detail/cast');
+
+        // 11.
+        $trailer_view = view('movies/detail/trailer');
+
+        // 12.
+        $main_view = view('movies/detail/main', [
+            'info'      => $info_view,
+            'cast'      => $cast_view,
+            'trailer'   => $trailer_view
+        ]);
+
+        // 13.
+        $html_wrapper = view('movies/html_wrapper', [
+            'header'  => view('movies/header'),
+            'footer'  => view('movies/footer'),
+            'content' => $main_view
+        ]);
+
+        return $html_wrapper;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'MovieController@index3');
+
+Route::get('/movies/detail/venom', 'MovieController@detail');
